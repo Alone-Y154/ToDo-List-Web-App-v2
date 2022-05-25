c218a3b2ce046637f8c1a01de441ede3422f8d85
Yashwanth Krishna <72192888+Alone-Y154@users.noreply.github.com>
2022-05-25 13:30:09 +0530

    error in port

--- app.js
+++ app.js
@@ -157,6 +157,6 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(rocess.env.PORT || 3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server started sucessfully");
 });
